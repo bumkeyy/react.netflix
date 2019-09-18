b7b6e67bf7e2a9f36a2920679cf92aeaa2ebd562
권기범 <kgbum2222@gmail.com>
2019-09-18 22:03:04 +0900

    Location Aware Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Header = styled.header`
@@ -24,6 +24,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 5px solid
+    ${props => (props.current ? '#3498db' : 'transparent')};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -33,18 +36,18 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-export default () => (
+export default withRouter(({ location: { pathname } }) => (
   <Header>
     <List>
-      <Item>
+      <Item current={pathname === '/'}>
         <SLink to='/'>Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === '/tv'}>
         <SLink to='/tv'>TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === '/search'}>
         <SLink to='/search'>Search</SLink>
       </Item>
     </List>
   </Header>
-);
+));

e4bc8cd15e34be22ce9508eafe1c1b8c5bff0c86
권기범 <kgbum2222@gmail.com>
2019-09-18 21:42:37 +0900

    Styled-Components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Router from './Router';
-import Header from './Header/Header';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
         <Router />
       </>
     );

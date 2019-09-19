c8eef629a5558e108d09c6cf1d4746ea532333b3
권기범 <kgbum2222@gmail.com>
2019-09-19 21:28:32 +0900

    Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { movieApi, tvApi } from '../../api';
 
 export default class extends React.Component {
   state = {
@@ -8,6 +9,40 @@ export default class extends React.Component {
     searchTerm: '',
     loading: false,
     error: null
+  };
+
+  componentDidMount() {
+    this.handleSubmit();
+  }
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== '') {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const {
+        data: { results: movieResults }
+      } = await movieApi.search(searchTerm);
+      const {
+        data: { results: tvResults }
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+        loading: true
+      });
+    } catch (error) {
+      this.setState({
+        error: "Can't find results."
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -19,6 +54,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }

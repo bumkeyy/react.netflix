68a15936afa631cee59c405e12b879c78b092277
권기범 <kgbum2222@gmail.com>
2019-09-19 23:13:27 +0900

    HomePresenter and Section Components

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from '../../Components/Section';
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, popular, upComing, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {upComing && upComing.length > 0 && (
+        <Section title='Upcoming Movies'>
+          {upComing.map(movie => movie.title)}
+        </Section>
+      )}
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title='Now Playing'>
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='Popular Movies'>
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   upComing: PropTypes.array,
   popular: PropTypes.array,
   loading: PropTypes.bool.isRequired,
-  error: PropTypes.stringㄷ
+  error: PropTypes.string
 };
 
 export default HomePresenter;

af9bf2b1e8d6e188f66a8203064df2693ecc5570
권기범 <kgbum2222@gmail.com>
2019-09-20 23:42:00 +0900

    Message Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from '../../Components/Loader';
 import Section from '../../Components/Section';
+import Message from '../../Components/Message';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -58,6 +59,13 @@ const SearchPresenter = ({
         )}
       </>
     )}
+    {error && <Message text={error} color='#e74c3c' />}
+    {tvResults &&
+      movieResults &&
+      tvResults.length === 0 &&
+      movieResults.length === 0 && (
+        <Message text='Nothing found' color='#95a5a6' />
+      )}
   </Container>
 );
 
